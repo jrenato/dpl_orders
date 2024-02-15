237e63979375902a7f88bfcc878be14970f995fe
karvalho <renato@karvalho.com.br>
2024-02-15 11:58:54 -0300

    Refactored customers models and admin

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,28 @@
+'''
+Admin for Customers app
+'''
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, CustomerAddress
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    '''
+    Admin for the Customer model
+    '''
+    list_display = ('name', 'short_name', 'email', 'cnpj', 'cpf',)
+    search_fields = ('name', 'short_name', 'email', 'cnpj', 'cpf', 'phone',)
+    #list_filter = ('created', 'modified')
+    list_per_page = 20
+
+
+@admin.register(CustomerAddress)
+class CustomerAddressAdmin(admin.ModelAdmin):
+    '''
+    Admin for the CustomerAddress model
+    '''
+    list_display = ('customer', 'street', 'number', 'city', 'state', 'zip_code',)
+    search_fields = ('customer__name', 'street', 'city', 'state', 'zip_code',)
+    list_filter = ('state',)
+    list_per_page = 20
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,58 @@
+'''
+Models for Customers app
+'''
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    '''
+    Customer model
+    '''
+    name = models.CharField('Nome', max_length=120)
+    slug = models.SlugField('Identificador', max_length=120, unique=True)
+    short_name = models.CharField('Nome reduzido', max_length=50, unique=True)
+    email = models.EmailField('E-mail', blank=True, unique=True)
+    cnpj = models.CharField('CNPJ', max_length=14, blank=True, unique=True)
+    cpf = models.CharField('CPF', max_length=11, blank=True, unique=True)
+    phone = models.CharField('Telefone', max_length=20)
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    class Meta:
+        '''
+        Meta options
+        '''
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class CustomerAddress(models.Model):
+    '''
+    CustomerAddress model
+    '''
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+
+    street = models.CharField('Logradouro', max_length=100)
+    number = models.CharField('Número', max_length=15)
+    city = models.CharField('Cidade', max_length=50, blank=True, null=True)
+    state = models.CharField('Estado', max_length=50, blank=True, null=True)
+    zip_code = models.CharField('CEP', max_length=10, blank=True, null=True)
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
+    class Meta:
+        '''
+        Meta options
+        '''
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
+        ordering = ['customer', 'street']
+
+    def __str__(self):
+        return f'{self.street}, {self.number}, {self.city} - {self.state}'

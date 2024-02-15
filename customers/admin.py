f25581d88138174dbdf2b9c767b2bece1efc2bf4
karvalho <renato@karvalho.com.br>
2024-02-15 12:08:15 -0300

    Added/updated address inlines

--- customers/admin.py
+++ customers/admin.py
@@ -6,6 +6,15 @@ from django.contrib import admin
 from .models import Customer, CustomerAddress
 
 
+class CustomerAddressInline(admin.StackedInline):
+    '''
+    CustomerAddressInline class
+    '''
+    model = CustomerAddress
+    readonly_fields = ('created', 'modified',)
+    extra = 1
+
+
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     '''
@@ -15,14 +24,5 @@ class CustomerAdmin(admin.ModelAdmin):
     search_fields = ('name', 'short_name', 'email', 'cnpj', 'cpf', 'phone',)
     #list_filter = ('created', 'modified')
     list_per_page = 20
-
-
-@admin.register(CustomerAddress)
-class CustomerAddressAdmin(admin.ModelAdmin):
-    '''
-    Admin for the CustomerAddress model
-    '''
-    list_display = ('customer', 'street', 'number', 'city', 'state', 'zip_code',)
-    search_fields = ('customer__name', 'street', 'city', 'state', 'zip_code',)
-    list_filter = ('state',)
-    list_per_page = 20
+    readonly_fields = ('created', 'modified',)
+    inlines = [CustomerAddressInline]
--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,14 +1,25 @@
+'''
+Admin for Suppliers app
+'''
 from django.contrib import admin
 from .models import Supplier, SupplierAddress
 
 
-class SupplierAddressInline(admin.StackedInline):  # You can use TabularInline for a more compact view
+class SupplierAddressInline(admin.StackedInline):
+    '''
+    SupplierAddressInline class
+    '''
     model = SupplierAddress
-    extra = 1  # Number of inline forms to display
+    readonly_fields = ('created', 'modified',)
+    extra = 1
 
 
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
+    '''
+    Admin for the Supplier model
+    '''
     list_display = ('name', 'short_name', 'contact_person', 'email', 'phone_number')
     search_fields = ('name', 'short_name', 'contact_person', 'email', 'phone_number')
+    readonly_fields = ('created', 'modified',)
     inlines = [SupplierAddressInline]

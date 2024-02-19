3a313ab1ab819031e50f2a90b584f4bd82380d9c
renato <renato@karvalho.com.br>
2024-02-19 10:03:55 -0300

    Refactored product group status into a list of tuples

--- product_groups/admin.py
+++ product_groups/admin.py
@@ -3,7 +3,7 @@ Admin for the Product Groups
 '''
 from django.contrib import admin
 
-from .models import ProductGroup, ProductGroupStatus
+from .models import ProductGroup
 
 
 @admin.register(ProductGroup)
@@ -15,14 +15,4 @@ class ProductGroupAdmin(admin.ModelAdmin):
     search_fields = ['name', 'status',]
     list_filter = ['status', 'customer_limit_date', 'supplier_limit_date',]
     date_hierarchy = 'supplier_limit_date'
-    readonly_fields = ['created', 'modified']
-
-
-@admin.register(ProductGroupStatus)
-class ProductGroupStatusAdmin(admin.ModelAdmin):
-    '''
-    Admin for the Product Group Status
-    '''
-    list_display = ['name',]
-    search_fields = ['name']
     readonly_fields = ['created', 'modified']
--- product_groups/models.py
+++ product_groups/models.py
@@ -2,27 +2,17 @@
 Models for the Product Group
 '''
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class ProductGroupStatus(models.Model):
-    '''
-    Model for the Product Group Status
-    '''
-    name = models.CharField('Nome', max_length=100)
-
-    created = models.DateTimeField('Criado em', auto_now_add=True)
-    modified = models.DateTimeField('Modificado em', auto_now=True)
-
-    class Meta:
-        '''
-        Meta options
-        '''
-        verbose_name = 'Status do Grupo de Produtos'
-        verbose_name_plural = 'Status dos Grupos de Produtos'
-        ordering = ['name']
-
-    def __str__(self):
-        return f'{self.name}'
+PRODUCT_GROUP_STATUS = [
+    ('PE', _('Pending')),
+    ('OR', _('Ordered')),
+    ('SE', _('In Separation')),
+    ('FI', _('Finished')),
+    ('CA', _('Cancelled')),
+    ('AR', _('Archived')),
+]
 
 
 class ProductGroup(models.Model):
@@ -30,11 +20,9 @@ class ProductGroup(models.Model):
     Model for the Product Group
     '''
     name = models.CharField('Nome', max_length=100)
+    status = models.CharField('Status', max_length=2, choices=PRODUCT_GROUP_STATUS, default='PE')
     products = models.ManyToManyField(
         'products.Product', verbose_name='Produtos', blank=True
-    )
-    status = models.ForeignKey(
-        ProductGroupStatus, on_delete=models.CASCADE, verbose_name='Status'
     )
 
     customer_limit_date = models.DateField('Data limite para o cliente', blank=True, null=True)

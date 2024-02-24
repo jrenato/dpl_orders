36d7da9603d5c3e217205016dc6d2497b676faee
karvalho <renato@karvalho.com.br>
2024-02-24 18:58:34 -0300

    Added mock supplier detail template

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,9 +1,14 @@
+'''
+This file is used to define the URL patterns for the suppliers app.
+'''
 from django.urls import path
-from .views import SupplierListView, SupplierCreateView, SupplierUpdateView, SupplierDeleteView
+from .views import SupplierListView, SupplierCreateView, SupplierUpdateView,\
+    SupplierDeleteView, SupplierDetailView
 
 app_name = 'suppliers'
 urlpatterns = [
     path('', SupplierListView.as_view(), name='list'),
+    path('detail/<int:pk>/', SupplierDetailView.as_view(), name='detail'),
     path('add/', SupplierCreateView.as_view(), name='create'),
     path('update/<int:pk>/', SupplierUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', SupplierDeleteView.as_view(), name='delete'),
--- suppliers/views.py
+++ suppliers/views.py
@@ -1,30 +1,50 @@
+'''
+Views for the suppliers app
+'''
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
 from .models import Supplier
 from .forms import SupplierForm
 
 
 class SupplierListView(ListView):
+    '''
+    List view for the Supplier model
+    '''
     model = Supplier
-    #template_name = 'suppliers/supplier_list.html'
     context_object_name = 'suppliers'
 
 
+class SupplierDetailView(DetailView):
+    '''
+    Detail view for the Supplier model
+    '''
+    model = Supplier
+    context_object_name = 'supplier'
+
+
 class SupplierCreateView(CreateView):
+    '''
+    Create view for the Supplier model
+    '''
     model = Supplier
     form_class = SupplierForm
-    #template_name = 'suppliers/supplier_form.html'
-    success_url = reverse_lazy('supplier_list')
+    success_url = reverse_lazy('suppliers:list')
 
 
 class SupplierUpdateView(UpdateView):
+    '''
+    Update view for the Supplier model
+    '''
     model = Supplier
     form_class = SupplierForm
-    #template_name = 'suppliers/supplier_form.html'
-    success_url = reverse_lazy('supplier_list')
+    success_url = reverse_lazy('suppliers:list')
 
 
 class SupplierDeleteView(DeleteView):
+    '''
+    Delete view for the Supplier model
+    '''
     model = Supplier
-    #template_name = 'suppliers/supplier_confirm_delete.html'
-    success_url = reverse_lazy('supplier_list')
+    success_url = reverse_lazy('suppliers:list')

7a9416c92e898fd3d0d7f34d89667d1f69ee79d0
karvalho <renato@karvalho.com.br>
2024-02-27 09:52:48 -0300

    Downgraded ODBC version from 18 to 17

--- install_odbc.sh
+++ install_odbc.sh
@@ -11,9 +11,9 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trust
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
 # optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers

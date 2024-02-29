2e6497e12feea8e3dcfbd38329fdd95dfc244b36
karvalho <renato@karvalho.com.br>
2024-02-29 12:05:09 -0300

    Improved suppliers views and templates

--- suppliers/forms.py
+++ suppliers/forms.py
@@ -1,20 +1,18 @@
+'''
+Forms for the suppliers app
+'''
 from django import forms
 from localflavor.br.forms import BRCPFField, BRCNPJField  # Import BRCPFField and BRCNPJField
 from .models import Supplier
 
 
 class SupplierForm(forms.ModelForm):
+    '''
+    Form for the Supplier model
+    '''
     cpf = BRCPFField(required=False)
     cnpj = BRCNPJField(required=False)
 
     class Meta:
         model = Supplier
-        fields = [
-            'name',
-            'short_name',
-            'cpf',
-            'cnpj',
-            'contact_person',
-            'email',
-            'phone_number',
-        ]
+        fields = '__all__'
--- suppliers/views.py
+++ suppliers/views.py
@@ -3,48 +3,54 @@ Views for the suppliers app
 '''
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from .models import Supplier
 from .forms import SupplierForm
 
 
-class SupplierListView(ListView):
+class SupplierListView(PermissionRequiredMixin, ListView):
     '''
     List view for the Supplier model
     '''
     model = Supplier
     context_object_name = 'suppliers'
+    permission_required = 'suppliers.view_supplier'
 
 
-class SupplierDetailView(DetailView):
+class SupplierDetailView(PermissionRequiredMixin, DetailView):
     '''
     Detail view for the Supplier model
     '''
     model = Supplier
     context_object_name = 'supplier'
+    permission_required = 'suppliers.view_supplier'
 
 
-class SupplierCreateView(CreateView):
+class SupplierCreateView(PermissionRequiredMixin, CreateView):
     '''
     Create view for the Supplier model
     '''
     model = Supplier
     form_class = SupplierForm
     success_url = reverse_lazy('suppliers:list')
+    permission_required = 'suppliers.add_supplier'
 
 
-class SupplierUpdateView(UpdateView):
+class SupplierUpdateView(PermissionRequiredMixin, UpdateView):
     '''
     Update view for the Supplier model
     '''
     model = Supplier
     form_class = SupplierForm
     success_url = reverse_lazy('suppliers:list')
+    permission_required = 'suppliers.change_supplier'
 
 
-class SupplierDeleteView(DeleteView):
+class SupplierDeleteView(PermissionRequiredMixin, DeleteView):
     '''
     Delete view for the Supplier model
     '''
     model = Supplier
     success_url = reverse_lazy('suppliers:list')
+    permission_required = 'suppliers.delete_supplier'

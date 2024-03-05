f1b99bdf135e2d5bf83769a144a4fe115931eb1e
karvalho <renato@karvalho.com.br>
2024-03-05 12:57:36 -0300

    Added order delete view override

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,14 @@
 '''
 Views for the orders app.
 '''
+from django.utils import timezone
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.db.models import Sum
 
-from .models import Order
+from .models import Order, ORDER_STATUS_CHOICES, OrderItem, ORDER_ITEM_STATUS_CHOICES
 from .forms import OrderForm
 
 
@@ -71,3 +73,29 @@ class OrderDeleteView(PermissionRequiredMixin, DeleteView):
     model = Order
     success_url = reverse_lazy('orders:list')
     permission_required = 'orders.delete_order'
+
+    # TODO: Finish this method
+    def delete(self, request, *args, **kwargs):
+        # Should mark the order as canceled instead of deleting it
+        # Also, should mark the order items as canceled
+
+        # order = self.get_object()
+        # order.canceled = timezone.now()
+        # canceled_status = [status[0] for status in ORDER_STATUS_CHOICES
+        #                    if status[1] == 'Canceled'][0]
+        # order.status = canceled_status
+        # order.save()
+
+        # item_finished_status = [status[0] for status in ORDER_ITEM_STATUS_CHOICES
+        #                         if status[1] == 'Finished'][0]
+        # item_cancel_status = [status[0] for status in ORDER_ITEM_STATUS_CHOICES
+        #                         if status[1] == 'Canceled'][0]
+
+        # # Mark all the order items as canceled, except the ones that are already finished
+        # for order_item in order.order_items.exclude(status=item_finished_status):
+        #     order_item.canceled = timezone.now()
+        #     order_item.status = item_cancel_status
+        #     order_item.save()
+
+        # Don't call the super method, just redirect to the success url
+        return HttpResponseRedirect(self.get_success_url())

2ecd329a7338e71f3ec5e5d7375683e4cb9bfa80
karvalho <renato@karvalho.com.br>
2024-03-19 10:02:27 -0300

    Improved order form

--- orders/forms.py
+++ orders/forms.py
@@ -1,16 +1,28 @@
 from django import forms
 
+from django.utils.translation import gettext_lazy as _
+
 from .models import Order
 from products.models import Product
 
 
 class OrderForm(forms.ModelForm):
-    products = forms.ModelMultipleChoiceField(
-        queryset=Product.objects.all(),
-        widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
-        required=True
+    # products = forms.ModelMultipleChoiceField(
+    #     queryset=Product.objects.all(),
+    #     widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
+    #     required=True
+    # )
+
+    # created = forms.DateTimeField(
+    #     required=False, label=_('Created at'),
+    #     widget=forms.TextInput(attrs={'disabled': 'disabled'})
+    # )
+
+    canceled = forms.DateTimeField(
+        required=False, label=_('Canceled at'),
+        widget=forms.TextInput(attrs={'disabled': 'disabled'})
     )
 
     class Meta:
         model = Order
-        fields = ('products',)
+        fields = '__all__'

de3d00b7939c3a21f3e009165b7440323b477165
karvalho <renato@karvalho.com.br>
2024-03-27 18:43:01 -0300

    Improved suppliers form

--- suppliers/forms.py
+++ suppliers/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext as _
 
 from localflavor.br.forms import BRCPFField, BRCNPJField
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, Submit
+from crispy_forms.layout import Layout, Fieldset, Submit, HTML, Div
 
 from .models import Supplier
 
@@ -15,8 +15,8 @@ class SupplierForm(forms.ModelForm):
     '''
     Form for the Supplier model
     '''
-    cpf = BRCPFField(required=False)
-    cnpj = BRCNPJField(required=False)
+    # cpf = BRCPFField(required=False)
+    # cnpj = BRCNPJField(required=False)
 
     class Meta:
         model = Supplier
@@ -25,18 +25,46 @@ class SupplierForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.add_input(Submit('submit', _('Save')))
-        # self.helper.layout = Layout(
-        #     Fieldset(
-        #         'Supplier',
-        #         'name',
-        #         'short_name',
-        #         'contact_person',
-        #         'email',
-        #         'phone_number',
-        #         'website',
-        #         'created',
-        #         'updated',
-        #     ),
-        #     Submit('submit', 'Submit'),
-        # )
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div('vl_id', css_class='col-md-3'),
+                    Div('mb_id', css_class='col-md-3'),
+                    Div('short_name', css_class='col-md-6'),
+                    css_class='row'
+                ),
+                Div(
+                    Div('name', css_class='col-md-6'),
+                    Div('company_name', css_class='col-md-6'),
+                    css_class='row'
+                ),
+                Div(
+                    Div('person_or_company', css_class='col-md-2'),
+                    Div('cnpj', css_class='col-md-5'),
+                    Div('cpf', css_class='col-md-5'),
+                    css_class='row'
+                ),
+                Div(
+                    Div('state_registration', css_class='col-md-4'),
+                    Div('municipal_registration', css_class='col-md-8'),
+                    css_class='row'
+                ),
+                Fieldset(
+                    _('Contact Details'),
+                    Div(
+                        Div('contact_person', css_class='col-md-6'),
+                        Div('phone_number', css_class='col-md-6'),
+                        css_class='row'
+                    ),
+                    Div(
+                        Div('email', css_class='col-md-6'),
+                        Div('emailnfe', css_class='col-md-6'),
+                        css_class='row'
+                    ),
+                ),
+            ),
+            Submit('submit', _('Save'), css_class="btn-success"),
+        )
+
+        self.fields['cpf'] = BRCPFField(required=False)
+        self.fields['cnpj'] = BRCNPJField(required=False)

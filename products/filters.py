e4f791683ee4574e82287d2da9dcc0cd4c62b30e
karvalho <renato@karvalho.com.br>
2024-04-18 12:20:44 -0300

    Set formhelper to product filter form

--- products/filters.py
+++ products/filters.py
@@ -1,23 +1,25 @@
 import django_filters
 
 from .models import Product
+from .forms import ProductFilterForm
 
 
 class ProductFilter(django_filters.FilterSet):
-    price = django_filters.NumberFilter()
-    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
-    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    # price = django_filters.NumberFilter()
+    # price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
+    # price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
 
-    release_year = django_filters.NumberFilter(field_name='release_date', lookup_expr='year')
-    release_year__gt = django_filters.NumberFilter(
-        field_name='release_date', lookup_expr='year__gt'
-    )
-    release_year__lt = django_filters.NumberFilter(
-        field_name='release_date', lookup_expr='year__lt'
-    )
+    # release_year = django_filters.NumberFilter(field_name='release_date', lookup_expr='year')
+    # release_year__gt = django_filters.NumberFilter(
+    #     field_name='release_date', lookup_expr='year__gt'
+    # )
+    # release_year__lt = django_filters.NumberFilter(
+    #     field_name='release_date', lookup_expr='year__lt'
+    # )
 
-    supplier__name = django_filters.CharFilter(lookup_expr='icontains')
+    # supplier__name = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Product
+        form = ProductFilterForm
         fields = ['price', 'release_date', 'supplier']
--- products/forms.py
+++ products/forms.py
@@ -4,6 +4,9 @@ Forms for the products app
 from django import forms
 
 from django_select2 import forms as s2forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit, Div, Button, Row, Column
+from crispy_forms.bootstrap import FormActions, InlineRadios
 
 from .models import Product
 
@@ -38,3 +41,29 @@ class ProductForm(forms.ModelForm):
         widgets = {
             'category': ProductCategoryWidget,
         }
+
+
+class ProductFilterForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = ['price', 'release_date', 'supplier']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('device_created', css_class='form-group col-12'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('device_id', css_class='form-group col-12'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('device_type', css_class='form-group col-6 mb-0'),
+                Column('device_group', css_class='form-group col-6 mb-0'),
+                css_class='form-row'
+            ),
+        )
